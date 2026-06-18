f302aea5fe2a2d73f84f680dc739722959df7d94
enji01 <enzi301206@gmail.com>
2026-06-18 14:28:57 +0700

    Tahap 3 Abstraction selesai

---
+++ Pendaftaran.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class Pendaftaran
+{
+    protected $id_pendaftaran;
+    protected $nama_calon;
+    protected $asal_sekolah;
+    protected $nilai_ujian;
+    protected $biaya_pendaftaran_dasar;
+    protected $jalur_pendaftaran;
+
+    public function __construct(
+        $id_pendaftaran,
+        $nama_calon,
+        $asal_sekolah,
+        $nilai_ujian,
+        $biaya_pendaftaran_dasar,
+        $jalur_pendaftaran
+    ) {
+        $this->id_pendaftaran = $id_pendaftaran;
+        $this->nama_calon = $nama_calon;
+        $this->asal_sekolah = $asal_sekolah;
+        $this->nilai_ujian = $nilai_ujian;
+        $this->biaya_pendaftaran_dasar = $biaya_pendaftaran_dasar;
+        $this->jalur_pendaftaran = $jalur_pendaftaran;
+    }
+
+    abstract public function hitungBiayaPendaftaran();
+
+    abstract public function tampilkanInfoJalur();
+}
+?>
